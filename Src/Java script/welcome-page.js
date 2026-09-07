241fc70bb03495e652b702c79bb5f154eacc7098
John Radebe <0307295707086@tpc.local>
2026-09-07 12:32:25 +0200

    created welcome page and learner register page java scripts

---
+++ Src/Java script/welcome-page.js
@@ -0,0 +1,4 @@
+document.getElementById("continueBtn").addEventListener("click", function () {
+    window.location.href = "learner-registration.html";
+//line 3 redirecs the page to the learner registration page
+});
